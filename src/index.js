e5ce9cb291cca7aae021d1cfb5ceca3c0b97d59b
ChristopherVuvu <vuvumumo93@gmail.com>
2023-04-10 09:21:29 +0300

    added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e)=>{
+    e.preventDefault();
+    handleToDo(e.target.value)
+  })
 });
+
+function handleToDo(todo){
+  let p = document.createElement('p')
+  p.textContent = todo;
+  document.querySelector('#list').appendChild(p)
+}
